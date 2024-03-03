2ef0fcb0e415f078e22029626d4a0f2bc7516898
Mikiel <151890791+Mikielai@users.noreply.github.com>
2024-03-04 02:19:30 +0800

    dashboard content
    kapag na click ako sa leave nag error sa database

--- stipayroll/stipayroll-main/payrollsystemsti/payrollsystemsti/AdminTabs/dashBoard.Designer.cs
+++ stipayroll/stipayroll-main/payrollsystemsti/payrollsystemsti/AdminTabs/dashBoard.Designer.cs
@@ -28,35 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.label1 = new System.Windows.Forms.Label();
+			this.lb_DashBoard = new System.Windows.Forms.Label();
+			this.pnl_Employee = new System.Windows.Forms.Panel();
+			this.lb_Employee = new System.Windows.Forms.Label();
+			this.pnl_Department = new System.Windows.Forms.Panel();
+			this.lb_Department = new System.Windows.Forms.Label();
+			this.pnl_Leave = new System.Windows.Forms.Panel();
+			this.lb_Leave = new System.Windows.Forms.Label();
+			this.pnl_Employee.SuspendLayout();
+			this.pnl_Department.SuspendLayout();
+			this.pnl_Leave.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// label1
+			// lb_DashBoard
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-			this.label1.Location = new System.Drawing.Point(127, 117);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(71, 17);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "dashboad";
+			this.lb_DashBoard.AutoSize = true;
+			this.lb_DashBoard.BackColor = System.Drawing.Color.Transparent;
+			this.lb_DashBoard.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lb_DashBoard.ForeColor = System.Drawing.SystemColors.Desktop;
+			this.lb_DashBoard.Location = new System.Drawing.Point(3, 9);
+			this.lb_DashBoard.Name = "lb_DashBoard";
+			this.lb_DashBoard.Size = new System.Drawing.Size(254, 51);
+			this.lb_DashBoard.TabIndex = 0;
+			this.lb_DashBoard.Text = "Jezhan Inc.";
+			// 
+			// pnl_Employee
+			// 
+			this.pnl_Employee.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+			this.pnl_Employee.Controls.Add(this.lb_Employee);
+			this.pnl_Employee.Location = new System.Drawing.Point(12, 82);
+			this.pnl_Employee.Name = "pnl_Employee";
+			this.pnl_Employee.Size = new System.Drawing.Size(274, 135);
+			this.pnl_Employee.TabIndex = 1;
+			this.pnl_Employee.Paint += new System.Windows.Forms.PaintEventHandler(this.pnl_Employee_Paint);
+			// 
+			// lb_Employee
+			// 
+			this.lb_Employee.AutoSize = true;
+			this.lb_Employee.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lb_Employee.ForeColor = System.Drawing.SystemColors.Desktop;
+			this.lb_Employee.Location = new System.Drawing.Point(59, 9);
+			this.lb_Employee.Name = "lb_Employee";
+			this.lb_Employee.Size = new System.Drawing.Size(141, 32);
+			this.lb_Employee.TabIndex = 0;
+			this.lb_Employee.Text = "Employee";
+			// 
+			// pnl_Department
+			// 
+			this.pnl_Department.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+			this.pnl_Department.Controls.Add(this.lb_Department);
+			this.pnl_Department.Location = new System.Drawing.Point(336, 82);
+			this.pnl_Department.Name = "pnl_Department";
+			this.pnl_Department.Size = new System.Drawing.Size(274, 135);
+			this.pnl_Department.TabIndex = 2;
+			// 
+			// lb_Department
+			// 
+			this.lb_Department.AutoSize = true;
+			this.lb_Department.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lb_Department.ForeColor = System.Drawing.SystemColors.Desktop;
+			this.lb_Department.Location = new System.Drawing.Point(59, 9);
+			this.lb_Department.Name = "lb_Department";
+			this.lb_Department.Size = new System.Drawing.Size(162, 32);
+			this.lb_Department.TabIndex = 0;
+			this.lb_Department.Text = "Department";
+			// 
+			// pnl_Leave
+			// 
+			this.pnl_Leave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+			this.pnl_Leave.Controls.Add(this.lb_Leave);
+			this.pnl_Leave.Location = new System.Drawing.Point(12, 242);
+			this.pnl_Leave.Name = "pnl_Leave";
+			this.pnl_Leave.Size = new System.Drawing.Size(274, 135);
+			this.pnl_Leave.TabIndex = 2;
+			// 
+			// lb_Leave
+			// 
+			this.lb_Leave.AutoSize = true;
+			this.lb_Leave.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lb_Leave.ForeColor = System.Drawing.SystemColors.Desktop;
+			this.lb_Leave.Location = new System.Drawing.Point(82, 9);
+			this.lb_Leave.Name = "lb_Leave";
+			this.lb_Leave.Size = new System.Drawing.Size(92, 32);
+			this.lb_Leave.TabIndex = 0;
+			this.lb_Leave.Text = "Leave";
 			// 
 			// dashBoard
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.label1);
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+			this.ClientSize = new System.Drawing.Size(920, 550);
+			this.Controls.Add(this.pnl_Leave);
+			this.Controls.Add(this.pnl_Department);
+			this.Controls.Add(this.pnl_Employee);
+			this.Controls.Add(this.lb_DashBoard);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "dashBoard";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "dashBoard";
+			this.Load += new System.EventHandler(this.dashBoard_Load);
+			this.pnl_Employee.ResumeLayout(false);
+			this.pnl_Employee.PerformLayout();
+			this.pnl_Department.ResumeLayout(false);
+			this.pnl_Department.PerformLayout();
+			this.pnl_Leave.ResumeLayout(false);
+			this.pnl_Leave.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
         }
 
-        #endregion
+		#endregion
 
-        private System.Windows.Forms.Label label1;
-    }
+		private System.Windows.Forms.Label lb_DashBoard;
+		private System.Windows.Forms.Panel pnl_Employee;
+		private System.Windows.Forms.Label lb_Employee;
+		private System.Windows.Forms.Panel pnl_Department;
+		private System.Windows.Forms.Label lb_Department;
+		private System.Windows.Forms.Panel pnl_Leave;
+		private System.Windows.Forms.Label lb_Leave;
+	}
 }
--- stipayroll/stipayroll-main/payrollsystemsti/payrollsystemsti/AdminTabs/dashBoard.cs
+++ stipayroll/stipayroll-main/payrollsystemsti/payrollsystemsti/AdminTabs/dashBoard.cs
@@ -1,12 +1,41 @@
-﻿using System.Windows.Forms;
+﻿using payrollsystemsti.EmployeeTabs;
+using System.Windows.Forms;
+using System;
 
 namespace payrollsystemsti
 {
 	public partial class dashBoard : Form
 	{
+		private leaveApplication leaveApplication;
 		public dashBoard()
 		{
 			InitializeComponent();
+			InitializeEventHandlers();
+
+		}
+
+		private void InitializeEventHandlers()
+		{
+			pnl_Leave.Click += Pnl_leave_Click;
+		}
+
+		private void Pnl_leave_Click(object sender, EventArgs e)
+		{
+			
+			leaveApplication = new leaveApplication();
+			leaveApplication.ShowDialog();
+		}	
+
+		private void dashBoard_Load(object sender, System.EventArgs e)
+		{
+
+		}
+
+
+
+		private void pnl_Employee_Paint(object sender, PaintEventArgs e)
+		{
+
 		}
 	}
 }
