30b01a0d50c890f8ec43c5b117d43532fc312f21
patatafiend <trimidal42@gmail.com>
2024-03-17 20:41:03 +0800

    Archive and Leave Management UI

--- payrollsystemsti/AdminTabs/leaveManagement.Designer.cs
+++ payrollsystemsti/AdminTabs/leaveManagement.Designer.cs
@@ -32,6 +32,8 @@
             this.dgLeaveID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dgLeaveName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.lbLM = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,12 +47,12 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dgLeaveID,
             this.dgLeaveName});
-            this.dataGridView1.Location = new System.Drawing.Point(51, 125);
+            this.dataGridView1.Location = new System.Drawing.Point(84, 202);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(833, 470);
+            this.dataGridView1.Size = new System.Drawing.Size(833, 342);
             this.dataGridView1.TabIndex = 3;
             // 
             // dgLeaveID
@@ -70,12 +72,36 @@
             this.lbLM.AutoSize = true;
             this.lbLM.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbLM.ForeColor = System.Drawing.Color.Crimson;
-            this.lbLM.Location = new System.Drawing.Point(45, 26);
+            this.lbLM.Location = new System.Drawing.Point(78, 29);
             this.lbLM.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbLM.Name = "lbLM";
             this.lbLM.Size = new System.Drawing.Size(278, 32);
             this.lbLM.TabIndex = 9;
             this.lbLM.Text = "Leave Management";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Teal;
+            this.btnAdd.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnAdd.Location = new System.Drawing.Point(84, 135);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(118, 44);
+            this.btnAdd.TabIndex = 25;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnUpdate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnUpdate.Location = new System.Drawing.Point(221, 135);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(118, 44);
+            this.btnUpdate.TabIndex = 26;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
             // 
             // leaveManagement
             // 
@@ -83,11 +109,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LavenderBlush;
             this.ClientSize = new System.Drawing.Size(974, 729);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.lbLM);
             this.Controls.Add(this.dataGridView1);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "leaveManagement";
             this.Text = "leaveManagement";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -102,5 +130,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dgLeaveID;
         private System.Windows.Forms.DataGridViewTextBoxColumn dgLeaveName;
         private System.Windows.Forms.Label lbLM;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
     }
 }
--- payrollsystemsti/EmployeeTabs/leaveApplication.Designer.cs
+++ payrollsystemsti/EmployeeTabs/leaveApplication.Designer.cs
@@ -339,6 +339,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
             this.ClientSize = new System.Drawing.Size(858, 658);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
